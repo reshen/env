fe48386ca120b58d64639bc8453d98b0bc052323
reshen <reshen@zensrc.com>
2009-11-27 18:10:54 -0700

    Adding option to use diffmerge

--- linux/bin/gitdiffwrap.sh
+++ linux/bin/gitdiffwrap.sh
@@ -3,7 +3,12 @@
 # diff is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode
 
-#echo Launching WinMergeU.exe: "`cygpath -aw $2`" "`cygpath -aw $5`"
+path="$(cygpath $1)"
+old="$(cygpath --mixed --absolute "$2")"
+new="$(cygpath --mixed --absolute "$5")"
 
-"/cygdrive/c/Program Files (x86)/WinMerge/WinMergeU.EXE" /e /ub /dl other /dr local "`cygpath -aw $2`" "`cygpath -aw $5`" | cat
+# Winmerge 
+#"/cygdrive/c/Program Files (x86)/WinMerge/WinMergeU.EXE" /e /ub /dl other /dr local "$old" "$new" | cat
 
+# Diffmerge 
+"C:/Program Files (x86)/DiffMerge/DiffMerge.exe" "$old" "$new" --title1="Old" --title2="New $path" | cat
